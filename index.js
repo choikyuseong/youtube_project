0d4be42b1964f765d7d964628c937e3474c4901e
kyuseong <kusung2230@naver.com>
2019-02-13 20:14:22 +0900

    app.get

--- index.js
+++ index.js
@@ -7,4 +7,25 @@ function handleListening(){
     console.log(`Listening on: http://localhost:${PORT}`);
 }
 
+const betweenHome = (req , res , next )=> {
+    console.log("between");
+    next();
+}
+
+const handleHome = (req , res , next) =>{
+    console.log("handle");
+    next();
+}
+
+const handleProfile = (req , res , next) =>{
+    console.log("handleProfile");
+    next();
+}
+
+// app.use(betweenHome);
+app.get("/", betweenHome,handleHome);
+
+app.get("/profile" , handleProfile);
+
+
 app.listen(PORT , handleListening);
