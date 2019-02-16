3211474bb9f01d132dcdf34131cdd53fa3fba879
kyuseong <kusung2230@naver.com>
2019-02-16 12:02:44 +0900

    db open and pugpage add

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,9 +1,12 @@
+import {Videos} from "../DB";
+
+
 export const videos  = (req , res) => res.render("videos",{pageTitle:"videos"});
 export const upload  =(req , res) => res.render("upload",{pageTitle:"upload"});
 export const video_detail  =(req , res) => res.render("video_detail",{pageTitle:"video_detail"});
 export const edit_video  =(req , res) => res.render("edit_video",{pageTitle:"edit_video"});
 export const delete_video  =(req , res) => res.render("delete_video",{pageTitle:"delete_video"});
-export const home  =(req , res) => res.render("home",{pageTitle:"home"});
+export const home  =(req , res) => res.render("home",{pageTitle:"home" , Videos});
 export const search  =(req , res) => {
     const{
         query: {term:searchingBy}
